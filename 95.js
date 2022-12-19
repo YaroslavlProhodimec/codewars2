ed5a3a8fcec371da475b23e5fba025aa8a875700
Yaroslav Myratov <yaroslav.muratov.97@inbox.ru>
2022-12-19 11:56:58 +0300

    12

---
+++ 95.js
@@ -0,0 +1,6 @@
+
+function howMuchWater(W,L,C){
+    if (C>2*L) return 'Too much clothes'
+    if (C<L) return 'Not enough clothes'
+    return  +((W * 1.1 ** (C - L)).toFixed(2))
+}
---
+++ 96.js
@@ -0,0 +1,5 @@
+function stairsIn20(s){
+    return   s
+        .reduce((arr, day) => arr.concat(...day), [])
+        .reduce((total, stairs) => total + stairs, 0) * 20
+}
